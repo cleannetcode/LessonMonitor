7a383bbec9b01aaed12df967fafa5560fd67947c
eniluck <eniluck@mail.ru>
2021-04-17 22:05:39 +0500

    add homeworkRoadMap controller

---
+++ LessonMonitor/LessonMonitor.Api/Controllers/RoadMapController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoadMapController : Controller
+    {
+        [HttpGet]
+        public Skill[] Get()
+        {
+            var random = new Random();
+            var skills = new List<Skill>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                var skill = new Skill();
+
+                skill.Id = i;
+                skill.ParentId = 0;
+                skill.SkillName = $"Skill_{i}";
+
+                skills.Add(skill);
+                for (int j = 1; j <= random.Next(3,8); j++)
+                {
+                    var child_skill = new Skill();
+                    child_skill.Id = i*100+j;
+                    child_skill.ParentId = i;
+                    child_skill.SkillName = $"Skill_{i}_{j}";
+
+                    skills.Add(child_skill);
+                }
+            }
+
+            return skills.ToArray();
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.Api/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string SkillName { get; set; }
+    }
+}
