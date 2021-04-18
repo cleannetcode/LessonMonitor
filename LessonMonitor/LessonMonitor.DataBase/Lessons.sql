c0231a6b9a210c453171170a0d071edcc8b98426
Trufanov Roman <pingvin1308@gmail.com>
2021-04-18 17:28:19 +0300

    добавил проект с базой данных

---
+++ LessonMonitor/LessonMonitor.DataBase/Lessons.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Lessons]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL, 
+    [Duration] INT NULL, 
+    [Members] NVARCHAR(MAX) NULL, 
+    [Difficulty] INT NULL, 
+    [Group] NVARCHAR(50) NULL
+)
---
+++ LessonMonitor/LessonMonitor.DataBase/Skills.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Skills]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL,
+    [Weight] INT NULL, 
+    [Subskills] NVARCHAR(MAX) NULL, 
+)
