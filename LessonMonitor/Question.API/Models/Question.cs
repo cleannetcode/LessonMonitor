69e2694a96c2381e82e9118e395fb7d4400871aa
oleg987 <frunt@DESKTOP-8L021IF>
2021-04-19 13:27:33 +0300

    Update Question Model; Create QuestionController with All/AllWithoutAnswer/Add/GiveAnswer actions

--- LessonMonitor/Question.API/Models/Question.cs
+++ LessonMonitor/Question.API/Models/Question.cs
@@ -8,12 +8,43 @@ namespace Question.API.Models
     public class Question
     {
         public Guid Id { get; private set; }
-        public string Theme { get; set; }
-        public string Answer { get; set; }
+        public string Theme { get; private set; }
+        public string Answer { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? AnsweredAt { get; private set; }        
 
-        public Question()
+        public Question(string theme)
         {
             Id = Guid.NewGuid();
+
+            if (!string.IsNullOrWhiteSpace(theme))
+            {
+                Theme = theme;
+            }
+            else
+            {
+                throw new ArgumentException(nameof(theme), "Can`t create empty question.");
+            }
+
+            CreatedAt = DateTime.Now;
+        }
+
+        public void GiveAnswer(string answer)
+        {
+            if (!string.IsNullOrWhiteSpace(Answer))
+            {
+                throw new ArgumentException("Question already have answer.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(answer))
+            {
+                Answer = answer;
+                AnsweredAt = DateTime.Now;
+            }
+            else
+            {
+                throw new ArgumentException(nameof(answer), "Answer can`t be empty.");
+            }
         }
     }
 }
