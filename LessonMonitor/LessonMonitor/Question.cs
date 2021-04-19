3ae2a15d4f929a3cf1573fdab1c739786fc57987
kalilask4 <kalilask4@gmail.com>
2021-04-20 01:33:32 +0300

    Controller QuestionsController added.

---
+++ LessonMonitor/LessonMonitor/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LessonMonitor.API
+{
+    public class Question
+    {
+        
+        private string text;
+        private string userName;
+
+
+        public string Text { get => text; set => text = value; }
+        public string UserName { get => userName; set => userName = value; }
+
+        public Question()
+        {
+        }
+
+
+    }
+}
