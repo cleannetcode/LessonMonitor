454c41455ff690202f317c4fd8913dcf8c41db50
oleg987 <frunt@DESKTOP-8L021IF>
2021-04-19 12:38:33 +0300

    Add Question Model

---
+++ LessonMonitor/Question.API/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question.API.Models
+{
+    public class Question
+    {
+        public Guid Id { get; private set; }
+        public string Theme { get; set; }
+        public string Answer { get; set; }
+
+        public Question()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
