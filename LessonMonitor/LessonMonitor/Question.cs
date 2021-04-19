02b036639211820c386a267a498a12b454148c2c
kalilask4 <kalilask4@gmail.com>
2021-04-20 01:35:27 +0300

    Property Datetime added for class Question

--- LessonMonitor/LessonMonitor/Question.cs
+++ LessonMonitor/LessonMonitor/Question.cs
@@ -11,13 +11,16 @@ namespace LessonMonitor.API
         
         private string text;
         private string userName;
+        private DateTime datetime;
 
 
         public string Text { get => text; set => text = value; }
         public string UserName { get => userName; set => userName = value; }
+        public DateTime Datetime { get => datetime; set => datetime = value; }
 
         public Question()
         {
+            Datetime = DateTime.Now;
         }
 
 
