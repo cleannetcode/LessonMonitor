b57068781f11a20ba34ce4704b9e87813bc292e5
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-20 16:06:31 +0500

    Modefy Achievements, Topics.

--- LessonMonitor/LessonMonitor.DataBase/Achievements.sql
+++ LessonMonitor/LessonMonitor.DataBase/Achievements.sql
@@ -2,6 +2,5 @@
 (
 	[Id] INT NOT NULL IDENTITY (1, 1), 
     [Name] NVARCHAR(MAX) NOT NULL, 
-    [MemberId] INT NOT NULL, 
     CONSTRAINT [PK_Achievements] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
--- LessonMonitor/LessonMonitor.DataBase/Topics.sql
+++ LessonMonitor/LessonMonitor.DataBase/Topics.sql
@@ -2,7 +2,5 @@
 (
 	[Id] INT NOT NULL, 
     [Theme] NVARCHAR(50) NULL, 
-    [HomeworkId] INT NULL, 
-    [QuestionId] INT NULL,
     CONSTRAINT [PK_Topics] PRIMARY KEY CLUSTERED ([Id] ASC),
 )
