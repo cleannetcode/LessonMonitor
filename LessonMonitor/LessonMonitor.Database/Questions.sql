dcaecaeacd9b5e6285a79d83ba38af8b978cfca5
kalilask4 <kalilask4@gmail.com>
2021-04-21 00:10:04 +0300

    Table questions added

---
+++ LessonMonitor/LessonMonitor.Database/Questions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Questions]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Text] NVARCHAR(MAX) NULL, 
+    [MemberId] INT NULL, 
+    [AnswerId] INT NOT NULL, 
+    [Hastags] VARBINARY(50) NOT NULL
+)
