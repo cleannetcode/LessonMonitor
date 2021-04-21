eaac82941b7283db4dcd5be90189ca3ebb7c1615
Misha902 <79799958+Misha902@users.noreply.github.com>
2021-04-21 20:23:48 +0300

    Добавил TimeCodesControllers, только не придумал как записывать таймкоды, поэтому они в формате DateTime

---
+++ LessonMonitor/test.API/Controllers/TimecodesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimecodesController : ControllerBase
+    {
+        public TimecodesController()
+        {
+
+        }
+        [HttpGet]
+        public string[] Get(string nameTimecode)
+        {
+            string result = "-1";
+            var timecodes = new List<string>();      
+            for (int i = 0; i < 5; i++)
+            {
+                var timecode = new Timecode()
+                {
+                    timecode = DateTime.Now
+                };
+                nameTimecode += i;
+                result = timecode.timecode.ToString() + ": " + nameTimecode;
+
+                timecodes.Add(result);
+            }
+
+            return timecodes.ToArray();
+        }
+    }
+}
