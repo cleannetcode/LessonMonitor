ea12f67891a172cd28b15a9d4839de1af26b4871
blood-mif <78865084+blood-mif@users.noreply.github.com>
2021-04-22 15:49:02 +0300

    Add controller Lesson

---
+++ LessonMonitor/LessonMonitor.API/Controllers/LessonsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class LessonsController : ControllerBase
+    {
+        [HttpGet]
+        public Lesson[] Get(string LessonName)
+        {
+            var lessons = new List<Lesson>();
+            for (int i = 0; i < 10; i++)
+            {
+                var lesson = new Lesson();
+                lesson.Theme = LessonName + i;
+                lesson.Number = i;
+
+                lessons.Add(lesson);
+            }
+            return lessons.ToArray();
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Lesson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API
+{
+    public class Lesson
+    {
+        public string Theme { get; set; }
+        public int Number { get; set; }
+    }
+}
