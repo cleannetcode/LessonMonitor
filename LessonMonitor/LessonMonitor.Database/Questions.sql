28870b9aa5929e003811dabd8d2bff6b5fbb6d29
kalilask4 <kalilask4@gmail.com>
2021-04-23 02:59:50 +0300

    fixed notes on the table Question

--- LessonMonitor/LessonMonitor.Database/Questions.sql
+++ LessonMonitor/LessonMonitor.Database/Questions.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Questions]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Text] NVARCHAR(MAX) NULL, 
-    [MemberId] INT NULL, 
-    [AnswerId] INT NOT NULL, 
+    [Text] NVARCHAR(MAX) NOT NULL, 
+    [MemberId] INT NOT NULL, 
+    [AnswerId] NVARCHAR(50) NULL, 
     [Hastags] VARBINARY(50) NOT NULL
 )
