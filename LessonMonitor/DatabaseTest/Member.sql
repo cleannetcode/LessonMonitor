ed5e43e09f8303481a6cfccf3827a6b4f0c05203
Misha902 <79799958+Misha902@users.noreply.github.com>
2021-04-23 13:24:33 +0300

    Добавил пять таблиц

---
+++ LessonMonitor/DatabaseTest/Member.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Member]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FullName] NVARCHAR(50) NOT NULL, 
+    [Age] INT NULL, 
+    [Link] NVARCHAR(MAX) NULL
+)
---
+++ LessonMonitor/DatabaseTest/Timecodes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Timecodes]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserName] NVARCHAR(20) NOT NULL,
+    [Name] NVARCHAR(30) NOT NULL, 
+    [Timecode] FLOAT NOT NULL
+    
+)
