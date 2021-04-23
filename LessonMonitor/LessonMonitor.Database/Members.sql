5a67c1df5ca5397c73bf8b90a24d309339dd37e3
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-04-23 19:03:51 +0300

    Create database

---
+++ LessonMonitor/LessonMonitor.Database/Members.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Members]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [NickName] NVARCHAR(20) NOT NULL, 
+    [FullName] NVARCHAR(100) NULL, 
+    [GithubProfile] NVARCHAR(50) NULL
+)
