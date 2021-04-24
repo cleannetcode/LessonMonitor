9ec3a84dfc6ecf1dbf1bfe6e32194df59a581481
Danyello <ulkiora.panda@gmail.com>
2021-04-24 18:34:00 +0600

    start asp.net

---
+++ WebApiCS/MyStart/Controllers/AboutWofController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStart.Controllers
+{
+    [Route("wof/about")]
+    public class AboutWofController : Controller
+    {
+        [Route("Show")]
+        public string Index(string id,string option)
+        {
+            return "About "+ id + " " + option;
+        }
+    }
+}
