b30bf11aec694be0e9e1945601f0ae9e947e8f92
eniluck <eniluck@mail.ru>
2021-04-24 22:08:35 +0500

    created entities for Lessons, Homeworks, Members

---
+++ LessonMonitor/LessonMonitor.Database/Lessons.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE [dbo].[Lessons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] varchar(200) NULL, 
+    [Theme] varchar(200) NULL, 
+    [LessonType] varchar(200) NULL, 
+    [StartDate] DATETIME NULL, 
+    [Duration] INT NULL 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Продолжительность ( секунды )',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = N'COLUMN',
+    @level2name = N'Duration'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Начало урока',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = N'COLUMN',
+    @level2name = N'StartDate'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Тип урока: Лекция, Общение в дискорде, хакатон',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = N'COLUMN',
+    @level2name = N'LessonType'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Основная тема урока',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = N'COLUMN',
+    @level2name = N'Theme'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Название урока',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'урок для участников по определенной теме на каких то площадках',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Lessons',
+    @level2type = NULL,
+    @level2name = NULL
---
+++ LessonMonitor/LessonMonitor.Database/Members.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE [dbo].[Members]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [LastName] varchar(200) NULL, 
+    [FirstName] varchar(200) NULL, 
+    [Patronomyc] varchar(200) NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Фамилия',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Members',
+    @level2type = N'COLUMN',
+    @level2name = N'LastName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Имя',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Members',
+    @level2type = N'COLUMN',
+    @level2name = N'FirstName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Отчество',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Members',
+    @level2type = N'COLUMN',
+    @level2name = N'Patronomyc'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Участники, таблица предназначена для однозначной идентификации участника',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Members',
+    @level2type = NULL,
+    @level2name = NULL
