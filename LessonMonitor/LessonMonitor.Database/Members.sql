6501913556884ee1ad692bcc373e9fe8a6bb1ef9
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-04-24 15:34:26 +1000

    Добавил скрипты создания таблиц

---
+++ LessonMonitor/LessonMonitor.Database/Members.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Members]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FullName] NVARCHAR(100) NOT NULL, 
+    [Age] INT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [Link] NVARCHAR(MAX) NULL, 
+    [CourseId] INT NULL FOREIGN KEY REFERENCES Courses(Id)
+)
---
+++ LessonMonitor/LessonMonitor.Database/Skills.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Skills]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Weight] INT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [MemberId] INT NOT NULL FOREIGN KEY REFERENCES Members(Id)
+)
