3c404d757eb479bb599313b2395fdc6c59418979
Danyello <ulkiora.panda@gmail.com>
2021-04-25 17:15:13 +0600

    added a model

---
+++ !WebApiCSLearn/LessonMonitor.API/Models/Announcement.cs
@@ -0,0 +1,23 @@
+﻿using LessonMonitor.API.Interfaces;
+using System;
+
+namespace LessonMonitor.API.Controllers
+{
+    public class Announcement : IAnnouncement
+    {
+        public DateTime AnnouncementTime { get; set; }
+        public string AnnouncementData { get; set; }
+        public string Header { get; set; }
+        public Announcement()
+        {
+
+        }
+
+        public Announcement(DateTime announcementTime, string announcementData,string header)
+        {
+            this.AnnouncementTime = announcementTime;
+            this.AnnouncementData = announcementData;
+            this.Header = header;
+        }
+    }
+}
