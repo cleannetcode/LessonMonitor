b18f39040649e554c3ddef81b45af951b73750e5
Danyello <ulkiora.panda@gmail.com>
2021-04-25 17:15:47 +0600

    is it necessary?

---
+++ !WebApiCSLearn/LessonMonitor.API/Models/AnnouncementControl.cs
@@ -0,0 +1,9 @@
+﻿using LessonMonitor.API.Interfaces;
+
+namespace LessonMonitor.API.Models
+{
+    public class AnnouncementControl
+    {
+        public IAnnouncement Announcement {get;set;}
+    }
+}
