582f23aefa1a4ff3ce95f9be6d256e5d7edff4f9
Trufanov Roman <pingvin1308@gmail.com>
2021-04-25 17:30:54 +0300

    создал sqlScripts с примерами sql команд

--- LessonMonitor/LessonMonitor.DataBase/Lessons.sql
+++ LessonMonitor/LessonMonitor.DataBase/Lessons.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Lessons]
 (
-    [Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NOT NULL, 
+    Id INT NOT NULL PRIMARY KEY, 
+    Name NVARCHAR(50) NOT NULL, 
     [StartDate] DATETIME2 NOT NULL, 
     [Duration] INT NULL, 
     [Members] NVARCHAR(MAX) NULL, 
---
+++ LessonMonitor/LessonMonitor.DataBase/VisitedLessons.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[VisitedLessons]
+(
+    [MemberId] INT NOT NULL, 
+    [LessonId] INT NOT NULL, 
+    PRIMARY KEY ([LessonId], [MemberId]), 
+    FOREIGN KEY (LessonId) REFERENCES Lessons(Id),
+    FOREIGN KEY (MemberId) REFERENCES Members(Id)
+)
