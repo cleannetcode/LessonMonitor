cd586e12b10c76d20d16268c0992c354fb97f0d3
mevgenka <mevgenka@gmail.com>
2021-05-06 08:58:59 +0300

    Initial commit
    HomeWork/sql/1

---
+++ LessonMonitor/LessonMonitor.DataBase/Lessons.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Lessons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL, 
+    [Duration] INT NULL, 
+    [Difficulty] INT NULL,   
+    [Group] NVARCHAR(50) NULL
+)
