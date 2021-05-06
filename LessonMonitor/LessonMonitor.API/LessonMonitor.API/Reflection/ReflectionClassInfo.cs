8ab5d09ab1c104eb41eef479e555d863554c9715
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-05-06 10:50:07 +0300

    Added ReflectionService for get metadata from classes into models namespace

---
+++ LessonMonitor/LessonMonitor.API/LessonMonitor.API/Reflection/ReflectionClassInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LessonMonitor.API.Reflection
+{
+    public class ReflectionClassInfo
+    {
+        public string ClassName { get; set; }
+        public ICollection<ClassProperties> ClassProperties { get; set; }
+    }
+
+    public class ClassProperties
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/LessonMonitor.API/Reflection/ReflectionService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API.Reflection
+{
+    public class ReflectionService : IReflectionService
+    {
+        public IEnumerable<ReflectionClassInfo> GetAllClassesInfo()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = assembly.FullName.Split(',')[0];
+            var namespaceName = $"{assemblyName}.Models";
+
+            var namespaceClasses = assembly.GetTypes()
+                .Where(x => (x.Namespace.Contains(namespaceName)))
+                .ToArray();
+
+            List<ReflectionClassInfo> classesInfo = new();
+
+            foreach (var item in namespaceClasses)
+            {
+                var fields = item.GetProperties();
+
+                ReflectionClassInfo classInfo = new()
+                {
+                    ClassName = item.Name,
+                    ClassProperties = new List<ClassProperties>()
+                };
+
+                foreach (var field in fields)
+                {
+                    classInfo.ClassProperties
+                        .Add(new ClassProperties
+                        {
+                            Name = field.Name,
+                            Type = field.PropertyType.FullName,
+                            Summary = ""
+                        });
+                }
+
+                classesInfo.Add(classInfo);
+                Console.WriteLine();
+            }
+
+
+            return classesInfo;
+        }
+    }
+}
