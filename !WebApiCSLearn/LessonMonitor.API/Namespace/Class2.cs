4e748ed457d64aedcf33d56fc2635ed2f9ff84d6
Danyello <ulkiora.panda@gmail.com>
2021-05-07 16:09:57 +0600

    classes for reflection

---
+++ !WebApiCSLearn/LessonMonitor.API/Namespace/Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReflectionAttributes.Namespace
+{
+    public class Class2
+    {
+        private int number = 322;
+        private string text = "text";
+        private readonly bool status;
+
+        public int Number { get => number; }
+        public string Text { get => text; }
+        public bool Status { get => status; }
+
+        public string Method(string text)
+        {
+            return text;
+        }
+        public int Method(int number)
+        {
+            return number;
+        }
+        public void ShowAllProperties()
+        {
+            Console.WriteLine($"{number}\t{text}\t{status}");
+        }
+    }
+}
