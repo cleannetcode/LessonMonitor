aa646f10710816ceea2cf45473400463e31d85c4
Danyello <ulkiora.panda@gmail.com>
2021-05-07 15:53:32 +0600

    attributes

---
+++ !WebApiCSLearn/ReflectionAttributes/Attributes/MyAnnouncementValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using LessonMonitor.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReflectionAttributes.Attributes
+{
+    public class MyAnnouncementValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is Announcement)
+                return true;
+
+            this.ErrorMessage = "announcement shouldn't be a null";
+            return false;
+        }
+    }
+}
