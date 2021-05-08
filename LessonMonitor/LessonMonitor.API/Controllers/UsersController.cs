4bb805644f26490ba53254ac5459e807601fa0c0
Danyello <ulkiora.panda@gmail.com>
2021-05-08 21:25:52 +0600

    ht2

---
+++ LessonMonitor/LessonMonitor.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+
+        [HttpGet]
+        public User[] Get(string userName)
+        {
+            var random = new Random();
+            var users = new List<User>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var user = new User();
+
+                user.Name = userName + i;
+                user.Age = random.Next(20, 51);
+
+                users.Add(user);
+            }
+
+            return users.ToArray();
+        }
+    }
+}
