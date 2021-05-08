acd73338737327fbe68ffde85f7d1bddc8e72726
Danyello <ulkiora.panda@gmail.com>
2021-05-08 20:25:42 +0600

    custom attribues

---
+++ !WebApiCSLearn/LessonMonitor.API/Attributes/MyAnnouncementValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using LessonMonitor.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReflectionAttributes.Attributes
+{
+    public class MyAnnouncementValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return
+                (value is Announcement announcement
+                && !string.IsNullOrEmpty(announcement.AnnouncementData)
+                && !string.IsNullOrEmpty(announcement.Header)
+                );
+        }
+    }
+}
