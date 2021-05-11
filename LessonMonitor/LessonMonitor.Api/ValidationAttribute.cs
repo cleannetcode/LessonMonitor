f386137417694c99e35287951f603a830b4cfc36
Федерягин Андрей <eniluck@mail.ru>
2021-05-11 17:55:38 +0500

    add stub for ValidationAttribute

---
+++ LessonMonitor/LessonMonitor.Api/ValidationAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    public class ValidationAttribute: Attribute
+    {
+
+    }
+}
