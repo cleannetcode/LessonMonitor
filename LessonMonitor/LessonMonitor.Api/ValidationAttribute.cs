35930550bfed1da6c219b18fb4745d2f408d4345
Федерягин Андрей <eniluck@mail.ru>
2021-05-11 17:55:38 +0500

    add stub for ValidationAttribute

---
+++ LessonMonitor/LessonMonitor.Api/ValidationAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ValidationAttribute: Attribute
+    {
+
+    }
+    // Подсмотреть можно зедсь https://referencesource.microsoft.com/#System.ComponentModel.DataAnnotations/DataAnnotations/RangeAttribute.cs
+}
