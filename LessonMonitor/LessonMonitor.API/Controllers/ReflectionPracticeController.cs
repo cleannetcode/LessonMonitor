631bbbc4c46e0d56af8fa60eebbde8c7da094145
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-05-12 12:45:14 +0500

    ReflectionPractice

---
+++ LessonMonitor/LessonMonitor.API/Controllers/ReflectionPracticeController.cs
@@ -0,0 +1,40 @@
+﻿using LessonMonitor.API.Models.MyNameSpace;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LessonMonitor.API.Controllers.MyNameSpace
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReflectionPracticeController : ControllerBase
+    {
+        public ReflectionPracticeController() { }
+
+        [HttpGet("model")]
+        public string[] GetHomeworkTypeProperties()
+        {
+            var getHomeworksType = typeof(Homeworks);
+
+            List<string> dataArray = new List<string>(); 
+
+            foreach (var prop in getHomeworksType.GetProperties())
+            {
+                dataArray.Add(prop.ToString());
+            }
+
+            foreach (var mInfo in getHomeworksType.GetMembers())
+            {
+                dataArray.Add(mInfo.ToString());
+            }
+
+            foreach (var ctor in getHomeworksType.GetConstructors())
+            {
+                dataArray.Add(ctor.Attributes.ToString());
+            }
+
+            dataArray.Add(getHomeworksType.ToString());
+
+            return dataArray.ToArray();
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Models/Homeworks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LessonMonitor.API.Models.MyNameSpace
+{
+    public class Homeworks
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Topic Topic { get; set; }
+        public User User { get; set; }
+        public string PrintHomework()
+        {
+            return $"Homework: {Name}, Grade: {Grade}, Theme: {Topic.Theme}, Student: {User.Name}";
+        }
+    }
+}
