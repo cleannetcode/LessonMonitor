fdb4ecfb4c823c3e699f2d6b3009d034ebc6f563
Alexander <trulander@mail.ru>
2021-05-20 22:15:28 +0500

    init my homework, asp net core middleware.

---
+++ LessonMonitor/LessonMonitor.API/CheckHeaderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace LessonMonitor.API
+{
+    public static class CheckHeaderExtension
+    {
+        public static IApplicationBuilder UseCheckHeader(this IApplicationBuilder app, string customHeader)
+        {
+            return app.UseMiddleware<CheckHeaderMiddleware>(customHeader);
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/CheckHeaderMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LessonMonitor.API
+{
+    public class CheckHeaderMiddleware
+    {
+        private RequestDelegate _next;
+        private readonly string _customHeader;
+
+        public CheckHeaderMiddleware(RequestDelegate next, string customHeader)
+        {
+            _next = next;
+            _customHeader = customHeader;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var customHeader = context.Request.Headers.SingleOrDefault(header => header.Key == _customHeader);
+
+            if (customHeader.Key == null)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Отсутствует кастомный заголовок: TestHeader");
+            }
+            else
+            {
+                _next.Invoke(context);
+            }
+        }
+    }
+}
