aa8e7f23207288b0011f2766c71dda528acc27a7
Alexander <trulander@mail.ru>
2021-05-21 13:44:24 +0500

    init momework 3, mssql.

---
+++ LessonMonitor/LessonMonitor.DataBase/homework/accounttypes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[accounttypes]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] NVARCHAR(50) NOT NULL, 
+    [role] INT NOT NULL
+)
---
+++ LessonMonitor/LessonMonitor.DataBase/homework/lessons.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[lessons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] NCHAR(10) NULL, 
+    [dateLesson] DATETIME NULL
+)
