a3fa418f8fa7d354f39a2bf2519770b6a403af47
Misha902 <79799958+Misha902@users.noreply.github.com>
2021-05-28 18:07:14 +0300

    Повторил разбиение проекта API на слои, настроил зависимости между проектами

---
+++ LessonMonitor/LessonMonitor.BusinessLogic/UsersService.cs
@@ -0,0 +1,25 @@
+﻿using LessonMonitor.Core;
+using System;
+
+namespace LessonMonitor.BusinessLogic
+{
+    public class UsersService : IUsersService
+    {
+        private IUsersRepository _usersRepository;
+        public UsersService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+        public User[] Get()
+        {
+            var users = _usersRepository.Get();
+
+            return users;
+        }
+
+        public void Create(User user)
+        {
+
+        }
+    }
+}
