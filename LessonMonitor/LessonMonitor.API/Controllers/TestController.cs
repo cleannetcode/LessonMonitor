ea8bc87c17f5536560ef4a2ff706c79c6f49d947
Федерягин Андрей <eniluck@mail.ru>
2021-06-08 11:40:40 +0500

    test controller

---
+++ LessonMonitor/LessonMonitor.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
