495bf4ac362c396331e9e9c0155474fab42b2259
Evgeniy <Evilroad36@gmail.com>
2021-06-12 16:41:38 +0300

    Добавил 2 класса WareHouse и CarDetail , добавил контролер CarDetailController и к нему 2 метода Get и Post. Еще добавил синглтон чтоб сохранялись результаты добавления

---
+++ LessonMonitor/HW_ASP_1/Controllers/CarDetailController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW_ASP_1.Controllers
+{
+		[ApiController]
+		[Route("Market/warehouse/[controller]")]
+	public class CarDetailController : ControllerBase
+	{
+		private readonly WareHouse _house;
+		public CarDetailController(WareHouse house)
+		{
+			_house = house;
+		}
+		[HttpGet("ListDetail")]
+		public List<CarDetail> Get()
+		{
+			return _house.Details;
+		}
+		[HttpPost("AddWareHose")]
+		public List<CarDetail> Add([FromQuery] CarDetail detail) 
+		{
+			_house.Details.Add(detail);
+			return _house.Details;
+		}
+	}
+}
