475dfe1e38341cf1582a1fbaa6c7dea111e90d46
Trufanov Roman <roman.trufanov@waveaccess.ru>
2021-07-17 20:03:10 +0300

    добавил editor конфиг с анализаторами

--- LessonMonitor/LessonMonitor.BusinessLogic/HomeworksService.cs
+++ LessonMonitor/LessonMonitor.BusinessLogic/HomeworksService.cs
@@ -1,51 +1,51 @@
+using System;
+using System.Threading.Tasks;
 using LessonMonitor.Core;
 using LessonMonitor.Core.Exceptions;
 using LessonMonitor.Core.Repositories;
 using LessonMonitor.Core.Services;
-using System;
-using System.Threading.Tasks;
 
 namespace LessonMonitor.BusinessLogic
 {
-	public class HomeworksService : IHomeworksService
-	{
-		public const string HOMEWORK_IS_INVALID = "Homework is invalid!";
-		private readonly IHomeworksRepository _homeworksRepository;
+    public class HomeworksService : IHomeworksService
+    {
+        public const string HOMEWORK_IS_INVALID = "Homework is invalid!";
+        private readonly IHomeworksRepository _homeworksRepository;
 
-		public HomeworksService(IHomeworksRepository homeworksRepository)
-		{
-			_homeworksRepository = homeworksRepository;
-		}
+        public HomeworksService(IHomeworksRepository homeworksRepository)
+        {
+            _homeworksRepository = homeworksRepository;
+        }
 
-		public async Task<int> Create(Homework homework)
-		{
-			// валидация
-			if (homework is null)
-			{
-				throw new ArgumentNullException(nameof(homework));
-			}
+        public async Task<int> Create(Homework homework)
+        {
+            // валидация
+            if (homework is null)
+            {
+                throw new ArgumentNullException(nameof(homework));
+            }
 
-			var isInvalid = homework.Link == null
-				|| string.IsNullOrWhiteSpace(homework.Title);
+            var isInvalid = homework.Link == null
+                || string.IsNullOrWhiteSpace(homework.Title);
 
-			if (isInvalid)
-			{
-				throw new BusinessException(HOMEWORK_IS_INVALID);
-			}
+            if (isInvalid)
+            {
+                throw new BusinessException(HOMEWORK_IS_INVALID);
+            }
 
-			var homeworkId = await _homeworksRepository.Add(homework);
+            var homeworkId = await _homeworksRepository.Add(homework);
 
-			return homeworkId;
-		}
+            return homeworkId;
+        }
 
-		public async Task<bool> Delete(int homeworkId)
-		{
-			if (homeworkId == default)
-				throw new ArgumentException(nameof(homeworkId));
+        public async Task<bool> Delete(int homeworkId)
+        {
+            if (homeworkId == default)
+                throw new ArgumentException("HomeworkId should be greater than 0", nameof(homeworkId));
 
-			await _homeworksRepository.Delete(homeworkId);
+            await _homeworksRepository.Delete(homeworkId);
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
