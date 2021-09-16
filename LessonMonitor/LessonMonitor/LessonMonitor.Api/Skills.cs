4a805b11e0f4c28654b236b067efa8ec07dcbabe
ZipP <maxm_99@mail.ru>
2021-09-17 00:01:51 +0300

    Выполнено задание №1 ДЗ. Жду замечаний

---
+++ LessonMonitor/LessonMonitor/LessonMonitor.Api/Skills.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    public class Skills
+    {
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string DurationOfUse { get; set; }
+
+       
+
+        
+    }
+}
