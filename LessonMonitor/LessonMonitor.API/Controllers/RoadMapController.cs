b4ec60699ac282bd83d3b03120caaaa40a119e65
artylaz <74875575+artylaz@users.noreply.github.com>
2021-10-12 03:30:06 +0500

    Выполнил второй пункт!

---
+++ LessonMonitor/LessonMonitor.API/Controllers/RoadMapController.cs
@@ -0,0 +1,43 @@
+﻿using LessonMonitor.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoadMapController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRoadMap(string nameRoadMap)
+        {
+            var rng = new Random();
+
+            var roadmaps = Enumerable.Range(1, 5).Select(index => new RoadMap
+            {
+                Name = $"Test{index}",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(5 + rng.Next(5,10)),
+                Tasks = new List<Task>()
+                {
+                    new Task()
+                    {
+                        Purpose = $"Test{index}",
+                        StartDate = DateTime.Now,
+                        EndDate = DateTime.Now.AddDays(1 + rng.Next(1,5))
+                    }
+                }
+            });
+
+            foreach (var roadmap in roadmaps)
+            {
+                if(roadmap.Name== nameRoadMap)
+                    return Ok(roadmap);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LessonMonitor.API.Models
+{
+    public class Task
+    {
+        public string Purpose { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
