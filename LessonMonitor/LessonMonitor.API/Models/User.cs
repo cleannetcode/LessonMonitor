570203f26f7ac8e865df28577eed33ec18b8fbb1
kalilask4 <kalilask4@gmail.com>
2021-10-26 23:49:45 +0300

    implementing own attributes from lesson

--- LessonMonitor/LessonMonitor.API/Models/User.cs
+++ LessonMonitor/LessonMonitor.API/Models/User.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System;
 
 namespace LessonMonitor.API
 {
@@ -21,15 +20,65 @@ namespace LessonMonitor.API
         [Description("Test Method")]
         public void Test([Description("First parametr")] string value)
         {
+        }
+    }
 
+    //validation error if field Name empty
+    //https://localhost:44391/Users/model?Name=&Age=3
+    //{"type":"https://tools.ietf.org/html/rfc7231#section-6.5.1","title":"One or more validation errors occurred.","status":400,
+    //"traceId":"00-f9405ecb95189f4bbc6cd7f069fdbba1-936f7e5742404841-00",
+    //"errors":{"Name":["The Name field is required."]
+
+    public class DescriptionAttribute : Attribute
+    {
+        public string Text { get; }
+
+        public DescriptionAttribute(string text)
+        {
+            Text = text;
+        }
+    }
+    
+    public class RangeAttribute : Attribute
+    {
+        public RangeAttribute(int minValue, int maxValue)
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+            
         }
 
+        public int MinValue { get; }
+        public int MaxValue { get; }
     }
-        //validation error if field Name empty
-        //https://localhost:44391/Users/model?Name=&Age=3
-        //{"type":"https://tools.ietf.org/html/rfc7231#section-6.5.1","title":"One or more validation errors occurred.","status":400,
-        //"traceId":"00-f9405ecb95189f4bbc6cd7f069fdbba1-936f7e5742404841-00",
-        //"errors":{"Name":["The Name field is required."]
 
+    public class RequiredAttribute: Attribute
+    {
+    }
 
+    //[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    //sealed class MyAttribute : System.Attribute
+    //{
+    //    // See the attribute guidelines at 
+    //    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    //    readonly string positionalString;
+
+    //    // This is a positional argument
+    //    public MyAttribute(string positionalString)
+    //    {
+    //        this.positionalString = positionalString;
+
+    //        // TODO: Implement code here
+
+    //        throw new System.NotImplementedException();
+    //    }
+
+    //    public string PositionalString
+    //    {
+    //        get { return positionalString; }
+    //    }
+
+    //    // This is a named argument
+    //    public int NamedInt { get; set; }
+    //}
 }
