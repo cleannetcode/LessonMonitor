4d03d722e590c5baffdc88de561558dd903eef48
kalilask4 <kalilask4@gmail.com>
2021-10-29 07:00:58 +0300

    using attribute to validate tittle model in the controller method (check title - have to start from digit)

--- LessonMonitor/LessonMonitor.API/Controllers/LessonsController.cs
+++ LessonMonitor/LessonMonitor.API/Controllers/LessonsController.cs
@@ -26,10 +26,55 @@ namespace LessonMonitor.API.Controllers
                 LessonId = index,
                 DateStart = DateTime.Now,
                 Tittle = "Lesson " + index + "." + "Topic " + topics[rng.Next(1, 5)],
-                Url = "https:/lessonurl" + index + DateTime.Now.DayOfWeek.ToString() + rng.Next(221,342)
+                Url = "https:/lessonurl" + index + DateTime.Now.DayOfWeek.ToString() + rng.Next(221, 342)
             })
                 .ToArray();
             return Ok(result);
         }
+
+
+        //test MyValidation
+        [HttpGet("model")]
+        public void GetModel([FromQuery] Lesson lesson)
+        {
+            var model = lesson.GetType();
+
+            foreach (var property in model.GetProperties())
+            {
+                foreach (var customAttribute in property.CustomAttributes)
+                {
+                    if (customAttribute.AttributeType.Name == "MyRequiredAttribute")
+                    {
+                        var value = property.GetValue(lesson);
+                        var specifiedValue = Convert.ChangeType(value, property.PropertyType);
+
+
+                        if (value == null)
+                        {
+                            throw new Exception($"{ property.Name}: { value }");
+                        }
+
+                    }
+                }
+
+                var validationAttribute = property.CustomAttributes;
+
+                if (property.Name == "Tittle")
+                {
+
+                    if (validationAttribute != null)
+                    {
+                        var value = property.GetValue(lesson);
+
+                        var isValueStartsDigit = int.TryParse(value.ToString().Substring(0, 1), out _);
+                        if (!isValueStartsDigit)
+                        {
+                            throw new Exception($"Tittle lesson have to start from lesson num. Vrong { property.Name}: { value } {MyValidation.ErrorMessage}");
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
