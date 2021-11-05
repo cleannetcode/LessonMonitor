53adca6b54a08226081162ec653749c3e87b4ace
kalilask4 <kalilask4@gmail.com>
2021-11-06 02:10:04 +0300

    add QuestionRepository to .DataAccess

---
+++ LessonMonitor/LessonMonitor.DataAccess/QuestionsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LessonMonitor.DataAccess
+{
+    public class QuestionsRepository 
+    {
+        public QuestionsRepository()
+        {
+
+        }
+
+        public object[] Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(object question)
+        {
+
+        }
+    }
+}
