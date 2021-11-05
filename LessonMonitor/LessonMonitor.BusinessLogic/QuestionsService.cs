7e8607dc3acc78a78de9d8aefc971f2faa32de5c
kalilask4 <kalilask4@gmail.com>
2021-11-06 02:09:15 +0300

    add QuestionService to .BusinessLogic

---
+++ LessonMonitor/LessonMonitor.BusinessLogic/QuestionsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.BusinessLogic
+{
+    public class QuestionsService
+    {
+        public object[] Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(object question)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
