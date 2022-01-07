dda2034722bc50f6df87d39fe7d62af7356cbab2
VitaliyTyu <VitaliyTyu@yandex.ru>
2022-01-07 14:24:46 +0500

    Написал все, что было на лекции

---
+++ LessonMonitor/LessonMonitorAPI/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonMonitorAPI
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
