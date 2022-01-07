ade8fa1eda3020e6a556a8a4939170e8c40dd362
VitaliyTyu <VitaliyTyu@yandex.ru>
2022-01-07 15:48:14 +0500

    Добавлен класс Lesson и контролер Lessons с 2я методами Get

---
+++ LessonMonitor/LessonMonitorAPI/Lesson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.Api
+{
+    public class Lesson
+    {
+        public int Number { get; set; }
+        public string Topic { get; set; }
+    }
+}
