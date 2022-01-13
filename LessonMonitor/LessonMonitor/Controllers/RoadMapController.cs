7afedaad2b11ce4bfc19f9d226a25cbb882ed0c4
Proger0014 <iakov56@yandex.ru>
2022-01-13 18:12:05 +0600

    return type of response methods changed to IActionResult

--- LessonMonitor/LessonMonitor/Controllers/RoadMapController.cs
+++ LessonMonitor/LessonMonitor/Controllers/RoadMapController.cs
@@ -20,41 +20,35 @@ namespace LessonMonitor.Controllers
         public IEnumerable<RoadMap> _roadMaps { get; set; }
         public Project _project { get; set; }
 
-        public RoadMapController()
-        {
-
-        }
-
         [HttpGet]
-        public IEnumerable<RoadMap> Get()
+        public IActionResult Get()
         {
             var genRoadMap = new GenerateRoadMap();
             _project = new Project("Выпуск продукта", new Person("Алексей", "C"), Status.InProgress, dateStart: new DateTime(2020, 05, 08), dateEnd: new DateTime(2020, 06, 16));
             _roadMaps = genRoadMap.GetRoadsMap(_project);
 
-            return _roadMaps;
+            return Ok(_roadMaps);
         }
 
         [HttpPut]
-        public bool Put(Decide decide, Status status, int element = 0)
+        public IActionResult Put(Decide decide, Status status, int element = 0)
         {
-            _roadMaps = Get();
+            // Получить значения переменных _project и _roadMaps
+            Get();
 
             // Если изменение выполнено - вернет true
             switch (decide)
             {
                 case Decide.RoadMap:
                     _roadMaps.ElementAt(element).Change(status);
-                    return true;
                     break;
                 
                 case Decide.Project:
                     _project.Change(status);
-                    return true;
                     break;
             }
 
-            return false;
+            return Ok(_roadMaps);
         }
     }
 }
