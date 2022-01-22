16a3004f48922230689d3a6a580a4b779f13241c
Proger0014 <iakov56@yandex.ru>
2022-01-22 19:58:35 +0600

    finished sql homework 1

---
+++ LessonMonitorDB/LessonMonitorDB/Lesson.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Lesson]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(30) NULL,
+    [Topic] INT NOT NULL, 
+    [Skill] INT NOT NULL
+)
---
+++ LessonMonitorDB/LessonMonitorDB/Member.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Member]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(150) NULL
+)
