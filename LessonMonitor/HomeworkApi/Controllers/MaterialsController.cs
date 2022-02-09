56d5b6f57abcbd3201742d0d44db7591b5fddc46
Kharchenko Michael <kharchenko.michael.a@gmail.com>
2022-02-09 14:05:41 +0200

    Add HomeworkApi project and Add Material Controller

---
+++ LessonMonitor/HomeworkApi/Controllers/MaterialsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialsController : ControllerBase
+    {
+        private readonly string[] topics = {"C#", "ASP.Net Core", "MSSQL", "Web Client"};
+
+        [HttpGet]
+        public IEnumerable<Material> Get()
+        {
+            var materials = new List<Material>();
+            foreach (var topic in topics)
+            {
+                materials.AddRange(Enumerable.Range(1, Random.Shared.Next(15, 30)).Select(item => new Material()
+                {
+                    LessonName = $"Lesson_{item}",
+                    Topic = topic,
+                    Duration = new TimeSpan(Random.Shared.Next(1, 3), Random.Shared.Next(1, 60),
+                        Random.Shared.Next(1, 60)),
+                    CreationTime = new DateTime(2021, Random.Shared.Next(1, 13), Random.Shared.Next(1, 29))
+                }));
+            }
+
+            return materials;
+        }
+    }
+}
