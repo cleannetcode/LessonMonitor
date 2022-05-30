abcd48b3642ff922f8e4697b93f212842408bbde
Kharchenko Michael <kharchenko.michael.a@gmail.com>
2022-05-30 14:53:38 +0300

    Add Homework 4

---
+++ LessonMonitor/LessonMonitor.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LessonMonitor.Domain
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Age { get; set; }
+    }
+}
