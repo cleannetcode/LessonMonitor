93318c1bd1748a929f9f441120bd2fe456d54b88
Evgeny Smirnov <e-smirnov@outlook.com>
2022-06-13 13:52:21 +0300

    add databases

---
+++ LessonMonitor/LessonMonitir.DataBase/Members.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Members]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FullName] NVARCHAR(50) NOT NULL, 
+    [Age] INT NULL, 
+    [email] NVARCHAR(50) NULL
+)
---
+++ LessonMonitor/LessonMonitir.DataBase/Skills.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Skills]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [Description] NVARCHAR(50) NULL, 
+    [Difficulty] NCHAR(10) NULL
+)
