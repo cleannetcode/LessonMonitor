3c5b7ce64acc6e7a5d80d67a70d70081ede30808
Viktor <xjennyx068@gmail.com>
2022-06-22 09:37:29 +0300

    добавление базового апи проекта и эксперименты с контроллерами

---
+++ LessonMonitor/LessonMonitor.API/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMonitor.API
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
