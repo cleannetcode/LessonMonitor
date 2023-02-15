7d9a702f3e552577dcbb3314902c699d9baa1717
opaga <k1rsider@yandex.ru>
2023-02-15 17:37:37 +0300

    added custom validation attribute and handled with reflection

---
+++ LessonMonitor/LessonMonitor.API/Attributes/ValidateQuestionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using LessonMonitor.API.Data;
+
+namespace LessonMonitor.API.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ValidateQuestionAttribute : Attribute
+    {
+        private readonly DateTime _minPostDate = new DateTime(2010, 1, 1);
+
+        public ValidateQuestionAttribute() {}
+
+        public string ErrorMessage { get; set; } = "Something gone wrong...";
+
+        public bool IsValid(object target)
+        {
+            return target is Question question
+                && !string.IsNullOrWhiteSpace(question.Author)
+                && !string.IsNullOrWhiteSpace(question.Title)
+                && (question.PostDate >= _minPostDate && question.PostDate <= DateTime.Now);
+        }
+    }
+}
---
+++ LessonMonitor/LessonMonitor.API/Data/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace LessonMonitor.API.Data
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
