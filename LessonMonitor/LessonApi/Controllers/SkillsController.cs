2ef7f2d4fa0dac4686c0d6683f405ec76003d1eb
Ragnar <etashiev@ad.salyk.kg>
2023-09-12 19:54:14 +0600

    #1 add porject api

---
+++ LessonMonitor/LessonApi/Controllers/SkillsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LessonApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class SkillsController : ControllerBase
+    {
+        [HttpGet("getSkills")]
+        public IActionResult GetSkillStatistic()
+        {
+
+            return Ok("norm");
+        }
+    }
+}
